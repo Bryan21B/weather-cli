ddc79c7f50dc3d213a8b9bbdf7cfed2252d066b8
Bryan Blanchot <Bryan21B@users.noreply.github.com>
2025-03-07 23:54:02 +0100

    feat: allow user to specify default city
    BREAKING CHANGE: weather now doesn't provide weather if default city not set

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,36 @@
 #!/usr/bin/env node
 
 import { Command } from "commander";
+import Conf from "conf";
 import { api } from "./api/api.js";
 import { formatters } from "./formatters.js";
 
+const config = new Conf({ projectName: "weather-cli" });
+
 // Declare the program
 const program = new Command();
+
+program
+  .command("set-city <city>")
+  .description("Set your default city")
+  .action((city) => {
+    config.set("city", city);
+    console.log(`Default city set to: ${city}`);
+  });
 
 // Add actions to the program
 program
   .command("now")
   .description("Provide current local weather")
   .action(async () => {
-    const weather = await api.getWeatherNow("Paris");
+    const city = config.get("city") as string;
+    if (!city) {
+      console.log(
+        "No city set. Use 'set-city' to configure one."
+      );
+      return;
+    }
+    const weather = await api.getWeatherNow(city);
     console.log(formatters.humanizeWeatherNow(weather));
   });
 
